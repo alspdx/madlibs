6feb7b25c07fa7577eae26d79255c973eb5a5d66
Adam Smith and Zachary Marston <zachary@>
2017-10-23 10:47:04 -0700

    change logic function in js to loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,14 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var holidayInput = $("input#holiday").val();
-    var buildingInput = $("input#building").val();
-    var verb1Input = $("input#verb1").val();
-    var creatureInput = $("input#creature").val();
-    var clothingInput = $("input#clothing").val();
-    var figureInput = $("input#figure").val();
-    var pluralNoun1Input = $("input#plural-noun1").val();
-    var pastTenseVerbInput = $("input#past-tense-verb").val();
-    var foodInput = $("input#food").val();
-    var pluralNoun2Input = $("input#plural-noun2").val();
+    event.preventDefault();
 
-    $(".holiday").text(holidayInput);
-    $(".building").text(buildingInput);
-    $(".verb1").text(verb1Input);
-    $(".creature").text(creatureInput);
-    $(".clothing").text(clothingInput);
-    $(".figure").text(figureInput);
-    $(".plural-noun1").text(pluralNoun1Input);
-    $(".past-tense-verb").text(pastTenseVerbInput);
-    $(".food").text(foodInput);
-    $(".plural-noun2").text(pluralNoun2Input);
+    var blanks = ["holiday", "building", "verb1", "creature", "clothing", "figure", "plural-noun1", "past-tense-verb", "food", "plural-noun2"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
-    event.preventDefault();
   });
-})
+});
